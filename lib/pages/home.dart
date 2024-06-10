ecae1dc49e372b47914d3ce20933cf37e89f2cff
Sarim Ahmed <C41f0N@protonmail.com>
2024-06-10 16:58:32 +0500

    added Workout Type Intensity Chart

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,3 +1,4 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -18,48 +19,155 @@ class _HomeState extends State<Home> {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
+                // Bar Chart Workout Type Intensity
                 Container(
                   height: MediaQuery.of(context).size.height * 0.37,
                   width: MediaQuery.of(context).size.width * 0.37,
                   decoration: BoxDecoration(
-                    color: Colors.red,
+                    color: Colors.grey[900],
                     borderRadius: BorderRadius.circular(30),
+                  ),
+                  alignment: Alignment.center,
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: BarChart(
+                      swapAnimationDuration: Duration(seconds: 1),
+                      BarChartData(
+                        alignment: BarChartAlignment.spaceEvenly,
+                        gridData: const FlGridData(show: false),
+                        borderData: FlBorderData(
+                          show: true,
+                          border: Border(
+                            bottom: BorderSide(color: Colors.grey[500]!),
+                            // left: BorderSide(color: Colors.grey[500]!),
+                          ),
+                        ),
+                        titlesData: const FlTitlesData(
+                          bottomTitles: AxisTitles(
+                            sideTitles: SideTitles(showTitles: false),
+                          ),
+                          rightTitles: AxisTitles(
+                            sideTitles: SideTitles(showTitles: false),
+                          ),
+                        ),
+                        barGroups: [
+                          BarChartGroupData(
+                            x: 0,
+                            barRods: [
+                              BarChartRodData(
+                                toY: 15,
+                                width: 10,
+                                color: Theme.of(context).colorScheme.primary,
+                                borderRadius: const BorderRadius.only(
+                                  topLeft: Radius.circular(5),
+                                  topRight: Radius.circular(5),
+                                ),
+                              )
+                            ],
+                          ),
+                          BarChartGroupData(
+                            x: 0,
+                            barRods: [
+                              BarChartRodData(
+                                toY: 10,
+                                width: 10,
+                                color: Theme.of(context).colorScheme.primary,
+                                borderRadius: const BorderRadius.only(
+                                  topLeft: Radius.circular(5),
+                                  topRight: Radius.circular(5),
+                                ),
+                              )
+                            ],
+                          ),
+                          BarChartGroupData(
+                            x: 0,
+                            barRods: [
+                              BarChartRodData(
+                                toY: 7.5,
+                                width: 10,
+                                color: Theme.of(context).colorScheme.primary,
+                                borderRadius: const BorderRadius.only(
+                                  topLeft: Radius.circular(5),
+                                  topRight: Radius.circular(5),
+                                ),
+                              )
+                            ],
+                          ),
+                          BarChartGroupData(
+                            x: 0,
+                            barRods: [
+                              BarChartRodData(
+                                toY: 5.5,
+                                width: 10,
+                                color: Theme.of(context).colorScheme.primary,
+                                borderRadius: const BorderRadius.only(
+                                  topLeft: Radius.circular(5),
+                                  topRight: Radius.circular(5),
+                                ),
+                              )
+                            ],
+                          ),
+                          BarChartGroupData(
+                            x: 0,
+                            barRods: [
+                              BarChartRodData(
+                                toY: 6,
+                                width: 10,
+                                color: Theme.of(context).colorScheme.primary,
+                                borderRadius: const BorderRadius.only(
+                                  topLeft: Radius.circular(5),
+                                  topRight: Radius.circular(5),
+                                ),
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
                 ),
 
+                // Workout Intensity History
                 Container(
                   height: MediaQuery.of(context).size.height * 0.37,
                   width: MediaQuery.of(context).size.width * 0.57,
                   decoration: BoxDecoration(
-                    color: Colors.red,
+                    color: Colors.grey[900],
                     borderRadius: BorderRadius.circular(30),
                   ),
+                  alignment: Alignment.center,
+                  child: const Text("Workout Intensity History"),
                 ),
               ],
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
+                // Consistency Calendar
                 Container(
                   height: MediaQuery.of(context).size.height * 0.47,
                   width: MediaQuery.of(context).size.width * 0.47,
                   decoration: BoxDecoration(
-                    color: Colors.red,
+                    color: Colors.grey[900],
                     borderRadius: BorderRadius.circular(30),
                   ),
+                  alignment: Alignment.center,
+                  child: const Text("Consistency Calender"),
                 ),
 
+                // Text Editor
                 Container(
                   height: MediaQuery.of(context).size.height * 0.47,
                   width: MediaQuery.of(context).size.width * 0.47,
                   decoration: BoxDecoration(
-                    color: Colors.red,
+                    color: Colors.grey[900],
                     borderRadius: BorderRadius.circular(30),
                   ),
+                  alignment: Alignment.center,
+                  child: const Text("Text Editor"),
                 ),
               ],
             ),
-            
           ],
         ),
       ),
