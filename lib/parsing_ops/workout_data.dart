acf85968d842f0b1275791ebce73c9322d5fd8df
Sarim Ahmed <C41f0N@protonmail.com>
2024-06-12 16:32:23 +0500

    selecting and displaying file contents

--- lib/parsing_ops/workout_data.dart
+++ lib/parsing_ops/workout_data.dart
@@ -4,6 +4,25 @@ import 'package:workout_dashboard/parsing_ops/workout.dart';
 class WorkoutData {
   late List<Workout> workouts;
   XFile? workoutLogFile;
+  String rawWorkoutLogString = "";
+
+  void getFile() async {
+    workoutLogFile = await openFile(acceptedTypeGroups: <XTypeGroup>[
+      const XTypeGroup(
+        label: "TXT",
+        extensions: ['txt'],
+      )
+    ]);
+
+    await readFile();
+  }
+
+  // Function to read data from file
+  Future<void> readFile() async {
+    if (workoutLogFile != null) {
+      rawWorkoutLogString = await workoutLogFile!.readAsString();
+    }
+  }
 
   void parseData() {
     print(rawWorkoutLogString);
