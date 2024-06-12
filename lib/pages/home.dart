c8d477576805f05241176974f5243c42d0cba345
Sarim Ahmed <C41f0N@protonmail.com>
2024-06-12 15:53:55 +0500

    responsiveness and formatting

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -25,7 +25,7 @@ class _HomeState extends State<Home> {
               children: [
                 // Bar Chart Workout Type Intensity
                 Container(
-                  height: MediaQuery.of(context).size.height * 0.37,
+                  height: MediaQuery.of(context).size.height * 0.32,
                   width: MediaQuery.of(context).size.width * 0.37,
                   decoration: BoxDecoration(
                     color: Colors.grey[900],
@@ -66,118 +66,131 @@ class _HomeState extends State<Home> {
                         SizedBox(
                           height: MediaQuery.of(context).size.height * 0.15,
                           width: MediaQuery.of(context).size.width * 0.3,
-                    child: BarChart(
+                          child: BarChart(
                             swapAnimationDuration:
                                 const Duration(milliseconds: 250),
-                      BarChartData(
-                        alignment: BarChartAlignment.spaceAround,
-                        gridData: const FlGridData(show: false),
-                        borderData: FlBorderData(
-                          show: true,
-                          border: Border(
-                            bottom: BorderSide(color: Colors.grey[500]!),
-                            // left: BorderSide(color: Colors.grey[500]!),
-                          ),
-                        ),
-                        titlesData: FlTitlesData(
-                          bottomTitles: AxisTitles(
-                            sideTitles: SideTitles(showTitles: false),
-                          ),
-                          rightTitles: AxisTitles(
-                            sideTitles: SideTitles(showTitles: false),
-                          ),
-                          leftTitles: AxisTitles(
-                            sideTitles: SideTitles(showTitles: false),
-                          ),
-                          topTitles: AxisTitles(
-                            sideTitles: SideTitles(
-                              reservedSize: 5,
-                              getTitlesWidget: (x, y) {
-                                return Text("dfdf");
-                              },
+                            BarChartData(
+                              alignment: BarChartAlignment.spaceAround,
+                              gridData: const FlGridData(show: false),
+                              borderData: FlBorderData(
+                                show: true,
+                                border: Border(
+                                  bottom: BorderSide(color: Colors.grey[500]!),
+                                  // left: BorderSide(color: Colors.grey[500]!),
+                                ),
+                              ),
+                              titlesData: FlTitlesData(
+                                bottomTitles: const AxisTitles(
+                                  sideTitles: SideTitles(showTitles: false),
+                                ),
+                                rightTitles: const AxisTitles(
+                                  sideTitles: SideTitles(showTitles: false),
+                                ),
+                                leftTitles: const AxisTitles(
+                                  sideTitles: SideTitles(showTitles: false),
+                                ),
+                                topTitles: AxisTitles(
+                                  sideTitles: SideTitles(
+                                    reservedSize: 5,
+                                    getTitlesWidget: (x, y) {
+                                      return const Text("dfdf");
+                                    },
+                                  ),
+                                ),
+                              ),
+                              barGroups: [
+                                BarChartGroupData(
+                                  x: 0,
+                                  barRods: [
+                                    BarChartRodData(
+                                      toY: 15,
+                                      width: MediaQuery.of(context).size.width *
+                                          0.03,
+                                      color:
+                                          Theme.of(context).colorScheme.primary,
+                                      borderRadius: const BorderRadius.only(
+                                        topLeft: Radius.circular(10),
+                                        topRight: Radius.circular(10),
+                                      ),
+                                    )
+                                  ],
+                                ),
+                                BarChartGroupData(
+                                  x: 0,
+                                  barRods: [
+                                    BarChartRodData(
+                                      toY: 10,
+                                      width: MediaQuery.of(context).size.width *
+                                          0.03,
+                                      color:
+                                          Theme.of(context).colorScheme.primary,
+                                      borderRadius: const BorderRadius.only(
+                                        topLeft: Radius.circular(10),
+                                        topRight: Radius.circular(10),
+                                      ),
+                                    )
+                                  ],
+                                ),
+                                BarChartGroupData(
+                                  x: 0,
+                                  barRods: [
+                                    BarChartRodData(
+                                      toY: 7.5,
+                                      width: MediaQuery.of(context).size.width *
+                                          0.03,
+                                      color:
+                                          Theme.of(context).colorScheme.primary,
+                                      borderRadius: const BorderRadius.only(
+                                        topLeft: Radius.circular(10),
+                                        topRight: Radius.circular(10),
+                                      ),
+                                    )
+                                  ],
+                                ),
+                                BarChartGroupData(
+                                  x: 0,
+                                  barRods: [
+                                    BarChartRodData(
+                                      toY: 5.5,
+                                      width: MediaQuery.of(context).size.width *
+                                          0.03,
+                                      color:
+                                          Theme.of(context).colorScheme.primary,
+                                      borderRadius: const BorderRadius.only(
+                                        topLeft: Radius.circular(10),
+                                        topRight: Radius.circular(10),
+                                      ),
+                                    )
+                                  ],
+                                ),
+                                BarChartGroupData(
+                                  x: 0,
+                                  barRods: [
+                                    BarChartRodData(
+                                      toY: 6,
+                                      width: MediaQuery.of(context).size.width *
+                                          0.03,
+                                      color:
+                                          Theme.of(context).colorScheme.primary,
+                                      borderRadius: const BorderRadius.only(
+                                        topLeft: Radius.circular(10),
+                                        topRight: Radius.circular(10),
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ],
                             ),
                           ),
                         ),
-                        barGroups: [
-                          BarChartGroupData(
-                            x: 0,
-                            barRods: [
-                              BarChartRodData(
-                                toY: 15,
-                                width: 35,
-                                color: Theme.of(context).colorScheme.primary,
-                                borderRadius: const BorderRadius.only(
-                                  topLeft: Radius.circular(10),
-                                  topRight: Radius.circular(10),
-                                ),
-                              )
-                            ],
-                          ),
-                          BarChartGroupData(
-                            x: 0,
-                            barRods: [
-                              BarChartRodData(
-                                toY: 10,
-                                width: 35,
-                                color: Theme.of(context).colorScheme.primary,
-                                borderRadius: const BorderRadius.only(
-                                  topLeft: Radius.circular(10),
-                                  topRight: Radius.circular(10),
-                                ),
-                              )
-                            ],
-                          ),
-                          BarChartGroupData(
-                            x: 0,
-                            barRods: [
-                              BarChartRodData(
-                                toY: 7.5,
-                                width: 35,
-                                color: Theme.of(context).colorScheme.primary,
-                                borderRadius: const BorderRadius.only(
-                                  topLeft: Radius.circular(10),
-                                  topRight: Radius.circular(10),
-                                ),
-                              )
-                            ],
-                          ),
-                          BarChartGroupData(
-                            x: 0,
-                            barRods: [
-                              BarChartRodData(
-                                toY: 5.5,
-                                width: 35,
-                                color: Theme.of(context).colorScheme.primary,
-                                borderRadius: const BorderRadius.only(
-                                  topLeft: Radius.circular(10),
-                                  topRight: Radius.circular(10),
-                                ),
-                              )
-                            ],
-                          ),
-                          BarChartGroupData(
-                            x: 0,
-                            barRods: [
-                              BarChartRodData(
-                                toY: 6,
-                                width: 35,
-                                color: Theme.of(context).colorScheme.primary,
-                                borderRadius: const BorderRadius.only(
-                                  topLeft: Radius.circular(10),
-                                  topRight: Radius.circular(10),
-                                ),
-                              )
-                            ],
-                          ),
-                        ],
-                      ),
+                      ],
                     ),
                   ),
                 ),
 
                 // Workout Intensity History
                 Container(
-                  height: MediaQuery.of(context).size.height * 0.37,
+                  height: MediaQuery.of(context).size.height * 0.32,
                   width: MediaQuery.of(context).size.width * 0.57,
                   decoration: BoxDecoration(
                     color: Colors.grey[900],
@@ -186,9 +199,9 @@ class _HomeState extends State<Home> {
                   alignment: Alignment.center,
                   child: Padding(
                     padding: EdgeInsets.fromLTRB(
-                      MediaQuery.of(context).size.width * 0.02,
-                      MediaQuery.of(context).size.height * 0.03,
-                      MediaQuery.of(context).size.width * 0.02,
+                      MediaQuery.of(context).size.width * 0.025,
+                      MediaQuery.of(context).size.height * 0.04,
+                      MediaQuery.of(context).size.width * 0.03,
                       MediaQuery.of(context).size.height * 0.04,
                     ),
                     child: Column(
@@ -200,7 +213,7 @@ class _HomeState extends State<Home> {
                           children: [
                             SizedBox(
                               width: MediaQuery.of(context).size.width * 0.23,
-                              height: MediaQuery.of(context).size.height * 0.04,
+                              height: MediaQuery.of(context).size.height * 0.05,
                               child: const AutoSizeText(
                                 "Set History",
                                 style: TextStyle(
@@ -212,14 +225,15 @@ class _HomeState extends State<Home> {
                             ),
                             DropdownButton(
                               items: [
-                                DropdownMenuItem(child: Text("Dumbell Curls"))
+                                const DropdownMenuItem(
+                                    child: Text("Dumbell Curls"))
                               ],
                               onChanged: (x) {},
                             ),
                           ],
                         ),
                         SizedBox(
-                          height: MediaQuery.of(context).size.height * 0.2,
+                          height: MediaQuery.of(context).size.height * 0.17,
                           width: MediaQuery.of(context).size.width * 0.5,
                           child: LineChart(
                             LineChartData(
@@ -278,7 +292,7 @@ class _HomeState extends State<Home> {
               children: [
                 // Consistency Calendar
                 Container(
-                  height: MediaQuery.of(context).size.height * 0.47,
+                  height: MediaQuery.of(context).size.height * 0.40,
                   width: MediaQuery.of(context).size.width * 0.27,
                   decoration: BoxDecoration(
                     color: Colors.grey[900],
@@ -288,8 +302,10 @@ class _HomeState extends State<Home> {
                   child: HeatMapCalendar(
                     colorMode: ColorMode.opacity,
                     showColorTip: false,
-                    size: min(MediaQuery.of(context).size.height * 0.05,
-                        MediaQuery.of(context).size.width * 0.1),
+                    size: min(
+                      MediaQuery.of(context).size.height * 0.47 * 0.6,
+                      MediaQuery.of(context).size.width * 0.27 * 0.1,
+                    ),
                     colorsets: const {10: Colors.green},
                     datasets: {
                       DateTime.now(): 10,
@@ -301,7 +317,7 @@ class _HomeState extends State<Home> {
                 ),
 
                 Container(
-                  height: MediaQuery.of(context).size.height * 0.47,
+                  height: MediaQuery.of(context).size.height * 0.40,
                   width: MediaQuery.of(context).size.width * 0.27,
                   decoration: BoxDecoration(
                     color: Colors.grey[900],
@@ -309,24 +325,42 @@ class _HomeState extends State<Home> {
                   ),
                   alignment: Alignment.center,
                   child: Padding(
-                    padding: const EdgeInsets.all(16.0),
+                    padding: EdgeInsets.fromLTRB(
+                      MediaQuery.of(context).size.width * 0.0125,
+                      MediaQuery.of(context).size.height * 0.02,
+                      MediaQuery.of(context).size.width * 0.015,
+                      MediaQuery.of(context).size.height * 0.02,
+                    ),
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
                         Column(
                           children: [
-                            SizedBox(
-                              width: MediaQuery.of(context).size.width * 0.23,
-                              height: MediaQuery.of(context).size.height * 0.1,
-                              child: const AutoSizeText(
-                                "Average Completion",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.w400,
-                                  fontSize: 200,
+                            Row(
+                              children: [
+                                SizedBox(
+                                  width:
+                                      MediaQuery.of(context).size.width * 0.01,
                                 ),
-                                textAlign: TextAlign.left,
-                              ),
+                                SizedBox(
+                                  width:
+                                      MediaQuery.of(context).size.width * 0.19,
+                                  height:
+                                      MediaQuery.of(context).size.height * 0.05,
+                                  child: const AutoSizeText(
+                                    "Average Completion",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.w400,
+                                      fontSize: 200,
+                                    ),
+                                    textAlign: TextAlign.left,
+                                  ),
+                                ),
+                              ],
                             ),
+                            SizedBox(
+                                height:
+                                    MediaQuery.of(context).size.height * 0.04),
                             const Text("This Week"),
                             const SizedBox(height: 10),
                             LinearProgressIndicator(
@@ -378,9 +412,9 @@ class _HomeState extends State<Home> {
                   ),
                 ),
 
-                // Text Editor
+                // Last Workout
                 Container(
-                  height: MediaQuery.of(context).size.height * 0.47,
+                  height: MediaQuery.of(context).size.height * 0.40,
                   width: MediaQuery.of(context).size.width * 0.37,
                   decoration: BoxDecoration(
                     color: Colors.grey[900],
